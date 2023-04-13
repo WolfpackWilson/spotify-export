07a9800d7b7fb3e7b3ff26c59634fcf2db2b88fb
TheEric960 <jack451wilson@gmail.com>
2023-04-13 18:43:35 -0400

    removed NOT NULLs

--- instances/create_table.sql
+++ instances/create_table.sql
@@ -1,61 +1,61 @@
 CREATE TABLE Tracks
 (
     id          TEXT PRIMARY KEY,
-    album_id    TEXT    NOT NULL,
-    disc_number INTEGER NOT NULL,
-    duration    INTEGER NOT NULL,
-    explicit    INTEGER NOT NULL,
+    album_id    TEXT NOT NULL,
+    disc_number INTEGER,
+    duration    INTEGER,
+    explicit    INTEGER,
     isrc        TEXT,
-    name        TEXT    NOT NULL,
-    popularity  INTEGER NOT NULL,
-    preview     TEXT    NOT NULL,
-    uri         TEXT    NOT NULL,
+    name        TEXT,
+    popularity  INTEGER,
+    preview     TEXT,
+    uri         TEXT,
     FOREIGN KEY (album_id) REFERENCES Albums (id)
 );
 CREATE TABLE AudioFeatures
 (
     id               TEXT PRIMARY KEY,
-    acousticness     REAL    NOT NULL,
-    danceability     REAL    NOT NULL,
-    energy           REAL    NOT NULL,
-    instrumentalnees REAL    NOT NULL,
-    key              INTEGER NOT NULL,
-    liveness         REAL    NOT NULL,
-    loudness         REAL    NOT NULL,
-    mode             INTEGER NOT NULL,
-    speechiness      REAL    NOT NULL,
-    tempo            REAL    NOT NULL,
-    time_signature   INTEGER NOT NULL,
-    valence          REAL    NOT NULL,
+    acousticness     REAL,
+    danceability     REAL,
+    energy           REAL,
+    instrumentalnees REAL,
+    key              INTEGER,
+    liveness         REAL,
+    loudness         REAL,
+    mode             INTEGER,
+    speechiness      REAL,
+    tempo            REAL,
+    time_signature   INTEGER,
+    valence          REAL,
     FOREIGN KEY (id) REFERENCES Tracks (id)
 );
 CREATE TABLE Albums
 (
     id                     TEXT PRIMARY KEY,
-    album_type             TEXT    NOT NULL,
-    total_tracks           INTEGER NOT NULL,
+    album_type             TEXT,
+    total_tracks           INTEGER,
     image_url              TEXT,
-    name                   TEXT    NOT NULL,
-    release_date           TEXT    NOT NULL,
-    release_date_precision TEXT    NOT NULL,
-    uri                    TEXT    NOT NULL,
-    copyrights             TEXT    NOT NULL,
-    label                  TEXT    NOT NULL,
-    popularity             INTEGER NOT NULL
+    name                   TEXT,
+    release_date           TEXT,
+    release_date_precision TEXT,
+    uri                    TEXT,
+    copyrights             TEXT,
+    label                  TEXT,
+    popularity             INTEGER
 );
 CREATE TABLE Artists
 (
     id         TEXT PRIMARY KEY,
     image_url  TEXT,
-    name       TEXT    NOT NULL,
-    popularity INTEGER NOT NULL,
-    uri        TEXT    NOT NULL
+    name       TEXT,
+    popularity INTEGER,
+    uri        TEXT
 );
 CREATE TABLE UserTracks
 (
     track_id TEXT NOT NULL,
-    added_by TEXT NOT NULL,
-    added_at TEXT NOT NULL,
+    added_by TEXT,
+    added_at TEXT,
     PRIMARY KEY (track_id, added_by),
     FOREIGN KEY (track_id) REFERENCES Tracks (id)
 );
@@ -65,24 +65,24 @@ CREATE TABLE Genres
 );
 CREATE TABLE AlbumArtists
 (
-    album_id  TEXT NOT NULL,
-    artist_id TEXT NOT NULL,
+    album_id  TEXT,
+    artist_id TEXT,
     PRIMARY KEY (album_id, artist_id),
     FOREIGN KEY (album_id) REFERENCES Albums (id),
     FOREIGN KEY (artist_id) REFERENCES Artists (id)
 );
 CREATE TABLE TrackArtists
 (
-    track_id  TEXT NOT NULL,
-    artist_id TEXT NOT NULL,
+    track_id  TEXT,
+    artist_id TEXT,
     PRIMARY KEY (track_id, artist_id),
     FOREIGN KEY (track_id) REFERENCES Tracks (id),
     FOREIGN KEY (artist_id) REFERENCES Artists (id)
 );
 CREATE TABLE ArtistGenres
 (
-    artist_id TEXT NOT NULL,
-    genre     TEXT NOT NULL,
+    artist_id TEXT,
+    genre     TEXT,
     PRIMARY KEY (artist_id, genre),
     FOREIGN KEY (artist_id) REFERENCES Artists (id),
     FOREIGN KEY (genre) REFERENCES Genres (genre)
